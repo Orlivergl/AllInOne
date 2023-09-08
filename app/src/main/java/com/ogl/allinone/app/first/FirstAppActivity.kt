c03f2206d0b5620b201271d201350d73fcd2f86e
Orliver <orlivergl230575@gmail.com>
2023-09-07 23:34:59 -0500

    Initial commit

---
+++ app/src/main/java/com/ogl/allinone/app/first/FirstAppActivity.kt
@@ -0,0 +1,12 @@
+package com.ogl.allinone.app.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ogl.allinone.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
